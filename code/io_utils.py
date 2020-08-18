abf506bda2d740351d36fa00b057c252d77d6018
iripatx <68219572+iripatx@users.noreply.github.com>
2020-08-18 13:13:20 +0200

    Functional data reading and storage

--- code/io_utils.py
+++ code/io_utils.py
@@ -3,6 +3,7 @@
 # required imports
 from pathlib import Path
 import numpy as np
+import pandas as pd
 import time
 
 # Defining global paths
@@ -10,33 +11,65 @@ root_path = Path.cwd().parent
 data_path = root_path / 'data' / 'MAREA_dataset'
 timings_path = data_path / 'Activity Timings'
 subjects_path = data_path / 'Subject Data_txt format'
+
+def get_database():
+    """
+    
+    """
     
 
-def read_all_data():
+def read_all_data(verbose = True):
     """
     read_all_data
-    Returns all of the database in a dictionary.
-    The dictionary splits in other two dictionaries for indoors and outdoors data
-    Inside of them, each entry represents a subject, 
-    containing the corresponding activities.
+    Reads the database and stores it as a pandas dataframe.
+    The dataframe is stored as a pickle (.pkl) file
+    This function should only be called once. Once the dataframe is stored, 
+    it can be loaded quickly using get_database
+    
+    Arguments:
+        verbose (boolean, default = True): If true, the function will display information
+            about the reading process
     """ 
     
+    # Take current time
+    start = time.time()
+        
     # Creating dictionary
     data = {"indoors":{} , "outdoors":{}}
     
     # INDOORS DATA
-    indoors_dic = read_indoors_data(test = True)
-        
+    if verbose: print('Reading indoors data...')
+    indoors_dic = read_indoors_data(verbose = verbose)
+    data['indoors'] = indoors_dic
+    
     # OUTDOORS DATA
-    outdoors_dic= read_outdoors_data()
+    if verbose: print('Reading outdoors data...')
+    outdoors_dic= read_outdoors_data(verbose = verbose)
+    data['outdoors'] = outdoors_dic
+    
+    # Converting dictionary to dataframe
+    db = pd.DataFrame.from_dict(data)
+    
+    # Storing database as a pickle file
+    db.to_pickle(data_path / 'database')
+    
+    # Take current time and measure computation time
+    end = time.time()
+    if verbose: print('Tiempo total empleado: ' + str(end - start))
+    
+    end
 
 
 
-def read_indoors_data(test = False):
+def read_indoors_data(test = False, verbose = True):
     """
+    read_indoors_data
     Returns data taken from indoors experiments
+    
     Arguments:
-        test(boolean, default = False): If true, only the first subject will be read
+        test (boolean, default = False): If true, only the first subject will be read
+        verbose (boolean, default = True): If true, the function will display information
+            about the reading process
     """
     
     # Reading indoor timings file to find the number of subjects
@@ -60,7 +93,7 @@ def read_indoors_data(test = False):
         if (i == 3):
             continue
         
-        # Take current time to measure computation time
+        # Take current time
         start = time.time()
         
         # Read timings line
@@ -104,12 +137,69 @@ def read_indoors_data(test = False):
         dic['flat_space']['Waist'] = Waist[0:timings[7]-1, :]
         dic['flat_space']['Wrist'] = Wrist[0:timings[7]-1, :]
         
-        # Take current time to measure computation time
+        # Take current time and measure computation time
         end = time.time()
-        print('Sujeto ' + str(i + 1) + ' procesado. Tiempo empleado: ' + str(end - start))
+        if verbose: print('Sujeto ' + str(i + 1) + ' procesado. Tiempo empleado: ' + str(end - start))
         
         # Storing subject
         indoor_dic[str(i + 1)] = dic
     
     return indoor_dic
+
+
+def read_outdoors_data(test = False, verbose = True):
+    """
+    read_outdoors_data
+    Returns data taken from outdoors experiments
+    
+    Arguments:
+        test(boolean, default = False): If true, only the first subject will be read
+        verbose (boolean, default = True): If true, the function will display information
+            about the reading process
+    """
+    
+    # Reading outdoor timings file to find the number of subjects
+    with (timings_path / 'Outdoor Experiment Timings.txt').open() as f_timings:
+        num_subjects = sum(1 for line in f_timings)
+        
+    # If test is true, only one subject will be processed
+    if test:
+        num_subjects = 1
+            
+    # Creating dictionary for all indoor subjects
+    outdoor_dic = {}
+    
+    # For each subject
+    for i in np.arange(num_subjects):
+        
+        # Take current time to measure computation time
+        start = time.time()
+        
+        # Creating dictionary for the subject
+        dic = {'street':{'LF':[], 'RF':[], 'Waist':[], 'Wrist':[]}}
+        
+        # Reading subject's data
+        LF_filename = 'Sub' + str(i + 12) + '_LF.txt'
+        RF_filename = 'Sub' + str(i + 12) + '_RF.txt'
+        Waist_filename = 'Sub' + str(i + 12) + '_Waist.txt'
+        Wrist_filename = 'Sub' + str(i + 12) + '_Wrist.txt'
+        LF = np.loadtxt(subjects_path / LF_filename, delimiter = ',', skiprows = 1)
+        RF = np.loadtxt(subjects_path / RF_filename, delimiter = ',', skiprows = 1)
+        Waist = np.loadtxt(subjects_path / Waist_filename, delimiter = ',', skiprows = 1)
+        Wrist = np.loadtxt(subjects_path / Wrist_filename, delimiter = ',', skiprows = 1)
+        
+        # Storing subject's data
+        dic['street']['LF'] = LF
+        dic['street']['RF'] = RF
+        dic['street']['Waist'] = Waist
+        dic['street']['Wrist'] = Wrist
+        
+        # Take current time to measure computation time
+        end = time.time()
+        if verbose: print('Sujeto ' + str(i + 12) + ' procesado. Tiempo empleado: ' + str(end - start))
+        
+        # Storing subject
+        outdoor_dic[str(i + 1)] = dic
+    
+    return outdoor_dic
         

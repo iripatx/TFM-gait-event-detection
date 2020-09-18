db8d6969ca1f40a66b9fe0dfb6b639550562e92e
iripatx <68219572+iripatx@users.noreply.github.com>
2020-09-18 12:11:11 +0200

    Functional multilabel LSTM

--- code/io_test.py
+++ code/io_test.py
@@ -4,12 +4,8 @@
 import peak_based_detection as pbd
 from metrics import *
 import io_utils
+import LSTM_detection
 
 #db = io_utils.get_database()
-#db = io_utils.read_labels(db)
 
-data = pbd.detect_events()
-
-accuracy = compute_accuracy(data)
-precision = compute_precision(data)
-recall = compute_recall(data)
+LSTM_detection.detect_events()
